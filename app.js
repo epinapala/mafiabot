e1d11c7621b0044c721bfe9abc9b36de68864e0f
epinapala <epinapala@ebay.com>
2016-07-30 01:45:19 -0700

    - Add the ability to store users in local storage. - Add shuffling functionality.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var Botkit = require('botkit');
 var _ = require('underscore');
+var COMMAND_DELIMITER = '!';
+var users = [];
+var util = require('./util');
 
 var controller = Botkit.slackbot({
   json_file_store: './storage'
@@ -7,7 +10,7 @@ var controller = Botkit.slackbot({
 
 var bot = controller.spawn({
   token: "xoxb-8526921072-OVtelBhvHrrmEvDPPC9EIDHQ"
-})
+});
 
 bot.startRTM(function (err, bot, payload) {
   if (err) {
@@ -15,7 +18,7 @@ bot.startRTM(function (err, bot, payload) {
   }
 });
 
-controller.hears(["="], ["direct_message", "direct_mention", "mention", "ambient"], function (bot, message) {
+controller.hears([COMMAND_DELIMITER + "init"], ["direct_message", "direct_mention", "mention", "ambient"], function (bot, message) {
   bot.api.channels.list({ channel: message.channel }, function (err, response) {
     var current_channel_id = message.channel;
     if (err) {
@@ -35,11 +38,15 @@ controller.hears(["="], ["direct_message", "direct_mention", "mention", "ambient
               bot.reply(message, "Unable to find user : " + id);
             } else {
               bot.reply(message, user.name);
+              controller.storage.users.save(user, function (err) {
+                if (err) {
+                  console.log("Unable to save user : " + user.name);
+                } else {
+                  console.log("Saved user : " + user.name);
+                }
+              });
             }
           });
-          /*
-           
-          */
         });
       } else {
         bot.reply(message, 'No channels returned from slack :(');
@@ -48,13 +55,13 @@ controller.hears(["="], ["direct_message", "direct_mention", "mention", "ambient
   });
 });
 
-controller.hears(["=start"], ["direct_message", "direct_mention", "mention", "ambient"], function (bot, message) {
+controller.hears([COMMAND_DELIMITER + "start"], ["direct_message", "direct_mention", "mention", "ambient"], function (bot, message) {
   askRoles = function (response, convo) {
-    convo.ask('What roles are you planning for? Please specify comma seperated!', function (response, convo) {
+    convo.ask('What roles are you planning to assign? Please specify comma seperated!', function (response, convo) {
       parseRoles(response, convo);
       convo.next();
     });
-  }
+  };
   parseRoles = function (response, convo) {
     var roles = response
       .text // extract actual message
@@ -63,26 +70,27 @@ controller.hears(["=start"], ["direct_message", "direct_mention", "mention", "am
       Function.prototype.call,
       String.prototype.trim
       );
-    convo.say("here are your roles assigned to members" + JSON.stringify(roles));
-  }
+    matchRoles(roles, convo);
+    convo.next();
+  };
+
+  matchRoles = function (roles, convo) {
+    controller.storage.users.all(function (err, all_user_data) {
+      if (err) {
+        convo.say('Oh no! Not able to read users list!')
+      } else {
+        var users = _.map(all_user_data, function (currentObject) {
+          return _.pick(currentObject, "name");
+        });
+        var shuffledUsers = util.fisherYatesShuffle(users);
+        var shuffledRoles = util.fisherYatesShuffle(roles);
+        _.each(users, function (user, index) {
+          user.role = shuffledRoles[index];
+        });
+        convo.say("here are your roles assigned to members" + JSON.stringify(users));
+      }
+    });
+  };
 
   bot.startConversation(message, askRoles);
 });
-
-function formatUptime(uptime) {
-  var unit = 'second';
-  if (uptime > 60) {
-    uptime = uptime / 60;
-    unit = 'minute';
-  }
-  if (uptime > 60) {
-    uptime = uptime / 60;
-    unit = 'hour';
-  }
-  if (uptime != 1) {
-    unit = unit + 's';
-  }
-
-  uptime = uptime + ' ' + unit;
-  return uptime;
-}
---
+++ util.js
@@ -0,0 +1,42 @@
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
+
+//https://en.wikipedia.org/wiki/Fisher–Yates_shuffle
+function fisherYatesShuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+module.exports = {
+    fisherYatesShuffle: fisherYatesShuffle,
+    formatUptime: formatUptime
+};
